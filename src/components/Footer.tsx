0b554a7939e5346a3619401a7092c7a1a2fe0dee
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-07-24 01:52:44 +0900

    Footer adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,39 @@
 export default function Footer() {
   return (
-    <footer className="grid sm:grid-cols-2 grid-cols-1 gap-4">
-      <div>
-        <h3 className="text-3xl font-bold">Rent a Tes</h3>
-        <p>We offers a big range of vehicles for all your driving needs. We have the perfect car to meet your needs.</p>
-        <a href="tel:1234567890">(123)-456-7890</a>
-        <br />
-        <a href = "mailto: rentates@gmail.com">Rentates@gmail.com</a>
-      </div>
+    // Center the footer
+    <footer className="flex justify-center">
+      <div className="grid md:grid-cols-2 grid-cols-1 xl:grid-cols-4 gap-10 m-14">
+        <div className="flex flex-col items-center md:items-start">
+          <h3 className="text-3xl font-bold">Rent a Tes</h3>
+          <p className="text-center md:text-start">We offer a variety of Tesla models for all your needs. Your perfect car is waiting for you.</p>
+          <a href="tel:1234567890">(123)-456-7890</a>
+          <a href="mailto: rentates@gmail.com">Rentates@gmail.com</a>
+        </div>
 
-      <div>
-        <h3 className="text-3xl font-bold">Working Hours</h3>
-        <p>Mon - Fri: 9:00AM - 9:00PM</p>
-        <p>Sat: 9:00AM - 19:00PM</p>
-        <p>Sun: Closed</p>
-      </div>
+        <div className="flex flex-col items-center md:items-start">
+          <h3 className="text-3xl font-bold">Working Hours</h3>
+          <p>Mon - Fri: 9:00AM - 9:00PM</p>
+          <p>Sat: 9:00AM - 19:00PM</p>
+          <p>Sun: Closed</p>
+        </div>
 
-      <div>
-        <h3 className="text-3xl font-bold">Company</h3>
-        <a href="#home">Orlando</a>
-        <a href="#home">Careers</a>
-        <a href="#home">Blog</a>
-        <a href="#home">Values</a>
-      </div>
+        <div className="flex flex-col items-center md:items-start">
+          <h3 className="text-3xl font-bold">Company</h3>
+          <a className="block" href="#home">Orlando</a>
+          <a className="block" href="#home">Careers</a>
+          <a className="block" href="#home">Blog</a>
+          <a className="block" href="#home">Values</a>
+        </div>
 
-      <div>
-        <h3 className="text-3xl font-bold">Subscription</h3>
-        <p>Subscribe for the latest news and updates</p>
-        <form>
-          <input type="email" placeholder="Enter Email Address" />
-          <button>Submit</button>
-        </form>
+        <div className="flex flex-col items-center md:items-start">
+          <h3 className="text-3xl font-bold">Subscription</h3>
+          <p>Subscribe for the latest news and updates</p>
+          <form>
+            <input className="border p-2 m-2" type="email" placeholder="Enter Email Address" />
+            <button className="border p-2">Submit</button>
+          </form>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
