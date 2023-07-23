9c2f9cc0b591b4fef95d4336110726f5f8fbb3ca
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-07-24 00:47:56 +0900

    Added favicon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 export default function Footer() {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <div>
+        <p><span>Tesla</span> Rental</p>
+
+        <p>We offers a big range of vehicles for all your driving needs. We have the perfect car to meet your needs.</p>
+        <a href="tel:1234567890">(123)-456-7890</a>
+        <br />
+        <a href = "mailto: rentates@gmail.com">Rentates@gmail.com</a>
+      </div>
+    </footer>
   )
 }
