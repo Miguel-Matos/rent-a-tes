bdb1a8adcb57877c3eb38a2f29d27ab27a31dd1c
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-07-28 16:54:11 +0900

    Forgot the re-enable the opacity

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -5,11 +5,9 @@ export default function BottomBanner() {
       <div className='bg-black text-white mt-16'>
         <div className='flex h-40 flex-col md:flex-row items-center justify-around'>
           <h1 className='text-4xl font-bold z-20'>Book a Tesla today!</h1>
-
           <div className='absolute overflow-hidden h-40 flex flex-col justify-center'>
-            <img className='pointer-events-none object-none' src={bottom} alt='bottom banner' />
+            <img className='pointer-events-none object-none opacity-40' src={bottom} alt='bottom banner' />
           </div>
-          
           <a className='text-4xl font-bold text-red-600 z-20' href="tel:1234567890">(123)-456-7890</a>
         </div>
       </div>
--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -3,12 +3,12 @@ import top from '../img/top2.png'
 export default function TopBanner(props: {name: string}) {
   return(
     <div className='w-fit overflow-hidden'>
-      {/* <div className='absolute top-0 left-0 min-w-[1500px] max-w-[4000px] overflow-hidden'>
-        <img className='opacity-10' src={top} alt="top banner" />
+      <div className='absolute top-0 left-0 min-w-[1500px] max-w-[4000px] overflow-hidden'>
+        <img className='opacity-20' src={top} alt="top banner" />
       </div>
       <div className='min-h-[250px] xl:min-h-[275px] 2xl:min-h-[300px]'>
         <h1 className='text-4xl font-bold'>{props.name}</h1>
-      </div> */}
+      </div>
     </div>
 
   )
