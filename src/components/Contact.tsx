9f0c026324a86fb93f42d30139d6a3ecee19decd
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-08-04 06:56:31 -0400

    completed contact layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope, faLocationDot, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+import world from '../img/world.svg'
+
 export default function Contact() {
 
   return(
-    <div className="flex flex-col items-center">
-      <h3 className="text-4xl font-bold text-center mb-16">Need additional information?</h3>
-      <p className="text-center">A team of skilled professionals with over 15 years of experience are happy to assist with any questions or concerns.</p>
-      <div className="flex flex-col gap-5 font-bold my-5 items-center">
-        <a href="tel:1234567890">(123)-456-7890</a>
-        <a href="mailto: rentates@gmail.com">Rentates@gmail.com</a>
-        <p>Orlando, Florida</p>
+    <div className="flex flex-col items-center lg:flex-row lg:gap-5 xl:gap-10 2xl:gap-14">
+      <div>
+        <h3 className="text-4xl font-bold text-center lg:text-left mb-10">Need additional information?</h3>
+        <p className="text-center lg:text-left mb-5 text-lg text-slate-600">A team of skilled professionals with over 15 years of experience are happy to assist with any questions or concerns.</p>
+        <div className="flex flex-col gap-5 font-bold my-5 items-center lg:items-start">
+          <a href="tel:1234567890"><FontAwesomeIcon icon={faPhone} /> (123)-456-7890</a>
+          <a href="mailto: rentates@gmail.com"><FontAwesomeIcon icon={faEnvelope} /> Rentates@gmail.com</a>
+          <p><FontAwesomeIcon icon={faLocationDot} /> Orlando, Florida</p>
+        </div>
       </div>
+
       <form className="flex flex-col w-full">
-        <label className="text-center font-bold py-5 text-2xl" htmlFor="name">Full Name</label>
+        <label className="text-center lg:text-left font-bold py-5 text-lg" htmlFor="name">Full Name<span className='text-red-500'>*</span></label>
         <input className="border bg-slate-100 p-5" type="text" id="name" placeholder="John Smith" required />
 
-        <label className="text-center font-bold py-5 text-2xl"  htmlFor="email">Email</label>
+        <label className="text-center lg:text-left font-bold py-5 text-lg"  htmlFor="email">Email<span className='text-red-500'>*</span></label>
         <input className="border bg-slate-100 p-5" type="email" id="email" placeholder="John@gmail.com" required />
 
-        <label className="text-center font-bold py-5 text-2xl" htmlFor="info">Tell us about it</label>
+        <label className="text-center lg:text-left font-bold py-5 text-lg" htmlFor="info">Tell us about it<span className='text-red-500'>*</span></label>
         <textarea className="border bg-slate-100 p-5" id="info" placeholder="Description" />
 
-        <button className=" bg-red-500 p-10 text-white font-bold text-2xl mt-10 mb-32 shadow-md hover:shadow-2xl rounded-md">Send Message</button>
+        <button className=" bg-red-500 p-5 text-white font-bold text-lg mt-10 mb-32 shadow-md hover:shadow-2xl focus:bg-red-600 rounded-md"><FontAwesomeIcon icon={faEnvelopeOpenText} /> Send Message</button>
       </form>
+      {/* <img className='absolute -z-10 pointer-events-none opacity-50' src={world} alt='world' /> */}
     </div>
 
   ) 
