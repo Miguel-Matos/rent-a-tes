503c8be783f2e60ee19351fb1ae3a9cced4201ef
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-08-06 14:21:57 -0400

    started models page commented out images

--- src/components/Models.tsx
+++ src/components/Models.tsx
@@ -1,7 +1,7 @@
-import mod3 from '../img/modelPage3.jpg'
-import y from '../img/modelPageY.jpg'
-import s from '../img/modelPageS.jpg'
-import x from '../img/modelPageX.jpg'
+// import mod3 from '../img/modelPage3.jpg'
+// import y from '../img/modelPageY.jpg'
+// import s from '../img/modelPageS.jpg'
+// import x from '../img/modelPageX.jpg'
 
 export default function Models() {
 
