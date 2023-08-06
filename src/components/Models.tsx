3cb29d500bdf9e25fac2052bbef2dbe582a0a940
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-08-06 14:19:55 -0400

    started models page

--- src/components/Models.tsx
+++ src/components/Models.tsx
@@ -1,3 +1,8 @@
+import mod3 from '../img/modelPage3.jpg'
+import y from '../img/modelPageY.jpg'
+import s from '../img/modelPageS.jpg'
+import x from '../img/modelPageX.jpg'
+
 export default function Models() {
 
   return(
