5f4e0f73f62cbbcf9dc7fad62ed473647c4fbca6
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-08-11 19:06:04 -0400

    updated calendars

--- src/components/DatePick.tsx
+++ src/components/DatePick.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
 import DatePicker from "react-datepicker";
+import { subDays } from 'date-fns';
 import 'react-datepicker/dist/react-datepicker.css'
 
 export default function DatePick() {
   const [startDate, setStartDate] = useState<any>(new Date());
   return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} minDate={subDays(new Date(), 0)} />
   );
 };
